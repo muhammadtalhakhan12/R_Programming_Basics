110875e104e1c6bf8a595b154f4bfea5267130c4
Muhammad Talha Khan <74019155+muhammadtalhakhan12@users.noreply.github.com>
2021-05-24 15:43:27 +0500

    did some practice

--- Control_Structures.r
+++ Control_Structures.r
@@ -1,1 +1,56 @@
+###==============================IF,Els if, Else ===============================###
+
+x <- 10
+y <- 0
+
+if (x > 9) {
+     y <- 2
+} else {
+     y <- 0
+}
+y ### as per the above statement value of y is changed it's equal to 2 ###
+x
+
+### you can write above statement like this as well ####
+y <- if (x > 9) {
+      2
+} else {
+     y <- 0
+}
+
+y 
+
+####========================FOR Loop========================================####
+
+for (i in 1:10) {
+     print (i)
+}
+
+
+x <- c("a","b","c","d")
+x
+
+for (i in 1:4) {
+     print (x[i])
+}
+
+for (i in seq_along(x)) {
+     print (x[i])
+}
+
+for (letter in x) {
+     print (letter)
+}
+for (i in 1:4) print (x[i])
+
+
+
+### Nested For loop example ####
+x <- matrix(1:25, 5, 5) 
+for(i in seq_len(nrow(x))) {
+     for(j in seq_len(ncol(x))) {
+          print(x[i, j])
+     }
+}
+x 
 
