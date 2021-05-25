e21b2b6b8ad229cc2fd9cc4dd6ffa05cb9d1ad9a
Muhammad Talha Khan <74019155+muhammadtalhakhan12@users.noreply.github.com>
2021-05-25 17:27:38 +0500

    done some exercises

--- Control_Structures.r
+++ Control_Structures.r
@@ -54,3 +54,22 @@ for(i in seq_len(nrow(x))) {
 }
 x 
 
+####========================While Loop========================================####
+
+count <- 0
+while (count < 10) {
+   print(count)
+   count <- count + 1
+}
+
+
+z <- 5
+while(z >= 3 && z <= 10) {
+   print(z)
+   coin <- rbinom(1, 1, 0.5)
+   if(coin == 1) { ## random walk 
+      z <- z + 1
+   } else {
+      z <- z - 1
+   }
+}
