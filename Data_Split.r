b2e2d6fe8e69f1efd93446bc6c95ac02b578cf78
Muhammad Talha Khan <74019155+muhammadtalhakhan12@users.noreply.github.com>
2021-05-27 15:28:46 +0500

    done some work

--- Data_Split.r
+++ Data_Split.r
@@ -1,1 +1,24 @@
+###===========================Data Split=======================================###
 
+x <- c(rnorm(10), runif(10), rnorm(10,1))
+f <- gl(3,10)
+x
+f
+
+split(x,f)
+lapply(split(x,f), mean)
+lapply(split(x,f), sd)
+
+
+library(datasets)
+head(airquality)
+
+s <- split(airquality,airquality$Month)
+s
+table(airquality$Month)
+
+lapply(s, function(x) colMeans(x[, c("Ozone", "Solar.R", "Wind")]))
+sapply(s, function(x) colSums(x[, c("Ozone", "Solar.R", "Wind")]))
+sapply(s, function(x) colMeans(x[, c("Ozone", "Solar.R", "Wind")], na.rm = TRUE))
+
+###==========================================================================###
